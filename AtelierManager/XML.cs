59f55edb5f90f230b283e5876767862b00571d18
Marcus André <ddtank.marcus@gmail.com>
2021-05-02 01:17:00 -0300

    Fix XML Parsing

--- AtelierManager/XML.cs
+++ AtelierManager/XML.cs
@@ -1,43 +1,51 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using System.Xml.XPath;
+using System.Web;
 
 namespace AtelierManager
 {
     class XML
     {
         byte[] Script;
-        public XML(byte[] Script) {
+        public XML(byte[] Script)
+        {
+            HtmlNode.ElementsFlags["str"] = HtmlElementFlag.Empty;
             this.Script = Script;
         }
 
-        XDocument Doc;
+        HtmlDocument Doc;
         public string[] Import() {
             using (var Strm = new MemoryStream(Script))
             {
-                Doc = XDocument.Load(Strm);
-                var Elms = Doc.Root.XPathSelectElements("//str[@Text]");
-                var Text = Elms.Select(x => x.Attribute("Text").Value).ToArray();
-                Doc.Declaration.Encoding = "UTF-8";
+                Doc = new HtmlDocument();
+                Doc.OptionOutputOriginalCase = true;
+                Doc.OptionWriteEmptyNodes = true;
+
+                Doc.Load(Strm);
+
+                var Elms = Doc.DocumentNode.SelectNodes("//str[@text]");
+                if (Elms == null)
+                    return null;
+
+                var Text = Elms.Select(x => HttpUtility.HtmlDecode(x.Attributes["Text"].Value)).ToArray();
                 return Text;
             }
         }
 
         public byte[] Export(string[] Content) {
 
-            var Elms = Doc.Root.XPathSelectElements("//str[@Text]").ToArray();
+            var Elms = Doc.DocumentNode.SelectNodes("//str[@text]").ToArray();
             for (int i = 0; i < Elms.Length; i++) {
-                Elms[i].Attribute("Text").Value = Content[i];
+                Elms[i].Attributes["Text"].Value = HttpUtility.HtmlEncode(Content[i]);
             }
 
             using (MemoryStream Stream = new MemoryStream())
             {
-                Doc.Save(Stream);
+                Doc.Save(Stream, Encoding.UTF8);
                 return Stream.ToArray();
             }
         }
